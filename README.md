869f97788bf0ef3a34ef8e82db75fe7ae07895a5
hieyou1 <31020711+hieyou1@users.noreply.github.com>
2024-02-01 19:20:35 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# emailsplit
+
+Are you only allowed to send a emails to a certain amount of recipients at once via your email account? Do you have express permission to send emails to more recipients? Then try this! It's very beta, but it will help you split a list of lots of emails into smaller lists of emails. Gets the job done!
---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("gen").onclick = () => {
+    let emails = document.getElementById("emails").value.split("\n");
+    let emailNum = parseInt(document.getElementById("number").value);
+    let splitMails = [];
+    let currentArr = [];
+    for (let i of emails) {
+        i = i.trim();
+        if (i != "" && (currentArr.push(i) == emailNum)) {
+            splitMails.push(currentArr);
+            currentArr = [];
+        }
+    }
+
+    document.getElementById("buttons").innerHTML = "";
+    for (let i in splitMails) {
+        document.getElementById("buttons").appendChild(document.createElement("br"));
+        let btn = document.createElement("button");
+        btn.classList.add("btn", "btn-secondary");
+        btn.onclick = () => navigator.clipboard.writeText(splitMails[i].join("\n"));
+        btn.innerText = `Copy list #${i}`;
+        document.getElementById("buttons").appendChild(btn);
+    }
+};
